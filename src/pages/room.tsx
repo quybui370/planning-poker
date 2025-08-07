37e6041417b6394aca25202f8ccfb18ca5cf139a
Justin <quybui370@gmail.com>
2025-08-08 01:13:59 +0700

    update logic

--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useSearchParams } from "react-router-dom";
 import { Header } from "../components/Header";
 import { PockerTable } from "../components/PockerTable";
 import { Welcome } from "./welcome";
@@ -6,8 +6,10 @@ import { Welcome } from "./welcome";
 export function Room() {
   const location = useLocation();
   const { userId } = location.state || {};
+  const [searchParams] = useSearchParams();
+  let roomId = searchParams.get("room");
 
-  if (!userId) {
+  if (!userId || !roomId) {
     return <Welcome />;
   }
 
--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -6,20 +6,19 @@ import { writeData } from "../services/firebase";
 
 export function Welcome() {
   const navigate = useNavigate();
-  const [name, setName] = useState("");
+  const [name, setName] = useState(localStorage.getItem("name") ?? "");
   const [searchParams] = useSearchParams();
   let roomId = searchParams.get("room");
-  let isHost = Boolean(!roomId).toString();
+  let isHost = Boolean(!roomId);
   roomId = roomId ?? nanoid(10);
-  const userId = nanoid(10);
+  const userId = localStorage.getItem("id") ?? nanoid(10);
 
   const handleClick = () => {
-    sessionStorage.setItem("name", name);
-    sessionStorage.setItem("id", userId);
+    localStorage.setItem("name", name);
+    localStorage.setItem("id", userId);
     writeData(`/${roomId}/${userId}`, {
       name,
       vote: "",
-      isHost,
     });
     navigate(`/planning-poker/?room=${roomId}`, {
       state: {
@@ -33,16 +32,22 @@ export function Welcome() {
       <Header />
       <div className="container mx-auto px-2 flex justify-center items-center grow">
         <div className="flex flex-col items-center w-96 bg-slate-700 rounded-2xl p-10 gap-7">
-          <h1 className="font-medium text-xl">Choose your display name</h1>
-          <input
-            onChange={(e) => setName(e.target.value)}
-            className="p-1 border-2 rounded w-full border-slate-500"
-          />
+          {!localStorage.getItem("name") ? (
+            <>
+              <h1 className="font-medium text-xl">Choose your display name</h1>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                className="p-1 border-2 rounded w-full border-slate-500"
+              />
+            </>
+          ) : (
+            <h1 className="font-medium text-xl">Welcome back</h1>
+          )}
           <button
             onClick={handleClick}
             className="bg-sky-600 hover:bg-sky-500 rounded-lg py-3 px-4 cursor-pointer"
           >
-            Continue to game
+            {isHost ? "Start a new game" : "Join room"}
           </button>
         </div>
       </div>
